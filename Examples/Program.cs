c29d9a7bca88c57b01e5d6233d6c34fabca368cf
ChrisDill <chris.rj.dill@gmail.com>
2023-05-20 08:26:07 +0100

    Store and run examples as callbacks

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Raylib_cs;
 using Examples.Core;
 using Examples.Shapes;
@@ -9,156 +10,171 @@ using Examples.Audio;
 
 namespace Examples
 {
+    public class ExampleList
+    {
+        public static Func<int>[] CoreExamples = new Func<int>[]
+        {
+            Camera2dPlatformer.Main,
+            Camera2dDemo.Main,
+            Camera3dFirstPerson.Main,
+            Camera3dFree.Main,
+            Camera3dMode.Main,
+            Picking3d.Main,
+            BasicScreenManager.Main,
+            BasicWindow.Main,
+            CustomLogging.Main,
+            DropFiles.Main,
+            InputGamepad.Main,
+            InputGestures.Main,
+            InputKeys.Main,
+            InputMouseWheel.Main,
+            InputMouse.Main,
+            InputMultitouch.Main,
+            RandomValues.Main,
+            ScissorTest.Main,
+            SmoothPixelPerfect.Main,
+            SplitScreen.Main,
+            StorageValues.Main,
+            VrSimulator.Main,
+            WindowFlags.Main,
+            WindowLetterbox.Main,
+            WorldScreen.Main,
+        };
+
+        public static Func<int>[] ShapesExamples = new Func<int>[]
+        {
+            BasicShapes.Main,
+            BouncingBall.Main,
+            CollisionArea.Main,
+            ColorsPalette.Main,
+            EasingsBallAnim.Main,
+            EasingsBoxAnim.Main,
+            EasingsRectangleArray.Main,
+            FollowingEyes.Main,
+            LinesBezier.Main,
+            LogoRaylibAnim.Main,
+            LogoRaylibShape.Main,
+            RectangleScaling.Main,
+        };
+
+        public static Func<int>[] TexturesExamples = new Func<int>[]
+        {
+            BackgroundScrolling.Main,
+            BlendModes.Main,
+            Bunnymark.Main,
+            DrawTiled.Main,
+            ImageDrawing.Main,
+            ImageGeneration.Main,
+            ImageLoading.Main,
+            ImageProcessing.Main,
+            ImageText.Main,
+            LogoRaylibTexture.Main,
+            MousePainting.Main,
+            NpatchDrawing.Main,
+            ParticlesBlending.Main,
+            TexturedCurve.Main,
+            Polygon.Main,
+            RawData.Main,
+            SpriteAnim.Main,
+            SpriteButton.Main,
+            SpriteExplosion.Main,
+            SrcRecDstRec.Main,
+            ToImage.Main,
+        };
+
+        public static Func<int>[] TextExamples = new Func<int>[]
+        {
+            FontFilters.Main,
+            FontLoading.Main,
+            FontSdf.Main,
+            FontSpritefont.Main,
+            FormatText.Main,
+            InputBox.Main,
+            RaylibFonts.Main,
+            RectangleBounds.Main,
+            WritingAnim.Main,
+        };
+
+        public static Func<int>[] ModelsExamples = new Func<int>[]
+        {
+            AnimationDemo.Main,
+            BillboardDemo.Main,
+            BoxCollisions.Main,
+            CubicmapDemo.Main,
+            ModelCubeTexture.Main,
+            FirstPersonMaze.Main,
+            GeometricShapes.Main,
+            HeightmapDemo.Main,
+            LoadingGltf.Main,
+            LoadingVox.Main,
+            LoadingDemo.Main,
+            MeshGeneration.Main,
+            MeshPicking.Main,
+            OrthographicProjection.Main,
+            SolarSystem.Main,
+            SkyboxDemo.Main,
+            WavingCubes.Main,
+            YawPitchRoll.Main,
+        };
+
+        public static Func<int>[] ShadersExamples = new Func<int>[]
+        {
+            BasicLighting.Main,
+            CustomUniform.Main,
+            Eratosthenes.Main,
+            Fog.Main,
+            HotReloading.Main,
+            HybridRender.Main,
+            JuliaSet.Main,
+            ModelShader.Main,
+            MultiSample2d.Main,
+            PaletteSwitch.Main,
+            PostProcessing.Main,
+            Raymarching.Main,
+            MeshInstancing.Main,
+            ShapesTextures.Main,
+            SimpleMask.Main,
+            Spotlight.Main,
+            TextureDrawing.Main,
+            TextureOutline.Main,
+            TextureWaves.Main,
+            WriteDepth.Main,
+        };
+
+        public static Func<int>[] AudioExamples = new Func<int>[]
+        {
+            ModulePlaying.Main,
+            MusicStreamDemo.Main,
+            RawStream.Main,
+            SoundLoading.Main,
+        };
+    }
+
     class Program
     {
         static unsafe void Main(string[] args)
         {
             Raylib.SetTraceLogCallback(&Logging.LogConsole);
-
-            RunCoreExamples();
-            RunShapesExamples();
-            RunTextureExamples();
-            RunTextExamples();
-            RunModelExamples();
-            RunShaderExamples();
-            RunAudioExamples();
+            RunExamples(ExampleList.CoreExamples);
+            RunExamples(ExampleList.ShapesExamples);
+            RunExamples(ExampleList.TexturesExamples);
+            RunExamples(ExampleList.TextExamples);
+            RunExamples(ExampleList.ModelsExamples);
+            RunExamples(ExampleList.ShadersExamples);
+            RunExamples(ExampleList.AudioExamples);
         }
 
-        static void RunCoreExamples()
+        static void RunExamples(Func<int>[] examples)
         {
-            Camera2dPlatformer.Main();
-            Camera2dDemo.Main();
-            Camera3dFirstPerson.Main();
-            Camera3dFree.Main();
-            Camera3dMode.Main();
-            Picking3d.Main();
-            BasicScreenManager.Main();
-            BasicWindow.Main();
-            CustomLogging.Main();
-            DropFiles.Main();
-            InputGamepad.Main();
-            InputGestures.Main();
-            InputKeys.Main();
-            InputMouseWheel.Main();
-            InputMouse.Main();
-            InputMultitouch.Main();
-            RandomValues.Main();
-            ScissorTest.Main();
-            SmoothPixelPerfect.Main();
-            SplitScreen.Main();
-            StorageValues.Main();
-            VrSimulator.Main();
-            WindowFlags.Main();
-            WindowLetterbox.Main();
-            WorldScreen.Main();
-        }
-
-        static void RunShapesExamples()
-        {
-            BasicShapes.Main();
-            BouncingBall.Main();
-            CollisionArea.Main();
-            ColorsPalette.Main();
-            EasingsBallAnim.Main();
-            // EasingsBoxAnim.Main();
-            EasingsRectangleArray.Main();
-            FollowingEyes.Main();
-            LinesBezier.Main();
-            LogoRaylibAnim.Main();
-            LogoRaylibShape.Main();
-            RectangleScaling.Main();
-        }
-
-        static void RunTextureExamples()
-        {
-            BackgroundScrolling.Main();
-            BlendModes.Main();
-            Bunnymark.Main();
-            DrawTiled.Main();
-            ImageDrawing.Main();
-            ImageGeneration.Main();
-            ImageLoading.Main();
-            ImageProcessing.Main();
-            ImageText.Main();
-            LogoRaylibTexture.Main();
-            MousePainting.Main();
-            NpatchDrawing.Main();
-            ParticlesBlending.Main();
-            Polygon.Main();
-            RawData.Main();
-            SpriteAnim.Main();
-            SpriteButton.Main();
-            SpriteExplosion.Main();
-            SrcRecDstRec.Main();
-            ToImage.Main();
-        }
-
-        static void RunTextExamples()
-        {
-            // Draw3d.Main();
-            FontFilters.Main();
-            FontLoading.Main();
-            FontSdf.Main();
-            FontSpritefont.Main();
-            FormatText.Main();
-            InputBox.Main();
-            RaylibFonts.Main();
-            RectangleBounds.Main();
-            WritingAnim.Main();
-        }
-
-        static void RunModelExamples()
-        {
-            AnimationDemo.Main();
-            BillboardDemo.Main();
-            BoxCollisions.Main();
-            CubicmapDemo.Main();
-            ModelCubeTexture.Main();
-            FirstPersonMaze.Main();
-            GeometricShapes.Main();
-            HeightmapDemo.Main();
-            LoadingGltf.Main();
-            LoadingVox.Main();
-            LoadingDemo.Main();
-            MeshGeneration.Main();
-            MeshPicking.Main();
-            OrthographicProjection.Main();
-            SolarSystem.Main();
-            SkyboxDemo.Main();
-            WavingCubes.Main();
-            YawPitchRoll.Main();
-        }
-
-        static void RunShaderExamples()
-        {
-            BasicLighting.Main();
-            CustomUniform.Main();
-            Eratosthenes.Main();
-            Fog.Main();
-            HotReloading.Main();
-            HybridRender.Main();
-            JuliaSet.Main();
-            ModelShader.Main();
-            MultiSample2d.Main();
-            PaletteSwitch.Main();
-            PostProcessing.Main();
-            Raymarching.Main();
-            MeshInstancing.Main();
-            ShapesTextures.Main();
-            SimpleMask.Main();
-            Spotlight.Main();
-            TextureDrawing.Main();
-            TextureOutline.Main();
-            TextureWaves.Main();
-            WriteDepth.Main();
-        }
-
-        static void RunAudioExamples()
-        {
-            ModulePlaying.Main();
-            MusicStreamDemo.Main();
-            RawStream.Main();
-            SoundLoading.Main();
+            var configFlags = Enum.GetValues(typeof(ConfigFlags));
+            foreach (var example in examples)
+            {
+                example.Invoke();
+                foreach (ConfigFlags flag in configFlags)
+                {
+                    Raylib.ClearWindowState(flag);
+                }
+            }
         }
     }
 }
